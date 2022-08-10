8b754262b084a303b2cbac1611a14c236b697805
James Mburu Githire <jamesmburug24@gmail.com>
2022-08-10 11:23:18 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+function superbowlWin(array) {
+    // Criteria of object to be returned
+    const findObj = (array) => array.result === "W"
+    const found = array.find(findObj); // returns object
+    return(typeof(found) === 'object' ? found.year : undefined);
+}
